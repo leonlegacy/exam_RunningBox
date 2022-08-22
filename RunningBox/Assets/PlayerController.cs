aad5a04639b0c08a1ea65d2afe525e8c505e9010
momouang <syes0923momo@gmail.com>
2022-08-22 20:07:34 +0800

    It runs now, brickch.

---
+++ RunningBox/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10;
+
+    [SerializeField]
+    //CharacterController controller;
+    Rigidbody rigi;
+
+    [SerializeField]
+    Animator anim;
+    
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(x, 0f, z);
+        
+        //controller.Move(new Vector3(x, 0f, z) * speed);
+        //rigi.MovePosition
+        rigi.velocity = new Vector3(x, rigi.velocity.y, z) * speed;
+
+        if (direction != Vector3.zero)
+            transform.forward = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rigi.velocity != Vector3.zero)
+            anim.SetBool("IsRunning", true);
+        else
+            anim.SetBool("IsRunning", false);
+    }
+}
